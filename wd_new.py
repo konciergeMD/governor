f864fe244c0c54e24b5e5637eb55c3314e604da2
mburket <mburket@gmail.com>
2016-05-04 15:07:09 -0400

    ETP-336:update watch dog script.

--- wd_new.py
+++ wd_new.py
@@ -2,25 +2,14 @@
 
 import subprocess, os
 
-os.environ['PATH'] += os.pathsep + '/usr/sbin'
 governor_start_cmd = [ '/bin/systemctl', 'start', 'governor' ]
 
-def pip_checker():
-    pid_file = "/pg_cluster/pgsql/9.4/data/postmaster.pid"
-    try:
-        f = open (pid_file, "r")
-        pid_num = int(f.readline().rstrip())
-        f.close()
-        return pid_num
-    except Exception, e:
-        raise e
-
 try:
-    pid_num = pip_checker()
+    pid_file = "/pg_cluster/pgsql/9.4/data/postmaster.pid"
+    f = open(pid_file, "r")
+    pid_num = int(f.readline().rstrip())
+    f.close()
     print pid_num
-    try:
-        os.kill(pid_num, 0)
-    except Exception as e:
-        subprocess.call(governor_start_cmd)
+    os.kill(pid_num, 0)
 except Exception, e:
-    raise e
+    subprocess.call(governor_start_cmd)
